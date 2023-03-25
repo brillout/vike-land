7b5589b0496d38b4068bc836dc9f438b912f476e
brillout <github.com@brillout.com>
2023-03-25 19:00:17 +0000

    Deploying to gh-pages from @ brillout/vike@e9941a3ec8f6654a8c5c0bd5575d57cf8260f0b6 🚀

---
+++ assets/embed.5be73f6d.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{H as r}from"./Hammer.c9d957a2.js";Array.from(document.querySelectorAll(".logo")).forEach(e=>{new r(e).init()});
--- assets/embed.c28756a5.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";import{H as t}from"./Hammer.73d24025.js";const e=new t(document.querySelector("#logo"));e.init();
