560582e19f3ff5f267090a8dbaa443587a125657
brillout <github.com@brillout.com>
2023-06-25 09:58:28 +0000

    Deploying to gh-pages from @ brillout/vike@deefcdbd4f97f2d65be7c99136127ad3662f9f58 🚀

--- assets/embed-4c85735c.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";import{H as t,a as o}from"./Hammer-6baf1064.js";const a={rotate:o({x:-.13,y:-6.63,z:-1.2}),translate:{x:-2.6,y:7,z:0}};Array.from(document.querySelectorAll(".logo")).forEach(r=>{const e=new t(r);e.perspective=a,e.init()});
