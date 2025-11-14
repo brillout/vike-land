c4f37b9afe041d9fade9cf7b949374fc58c44856
Romuald Brillout <git@brillout.com>
2025-11-14 18:42:56 +0100

    Revert "copilot"
    This reverts commit 5f047f1a85a7fb60390ff4df0534f1ed828c137d.

--- index/anchor-scrolling.ts
+++ index/anchor-scrolling.ts
@@ -1,7 +1,6 @@
 import './anchor-scrolling.css'
 
 anchorScrolling()
-themeToggle()
 
 // Copied & adapted from https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link/49910424#49910424
 function anchorScrolling() {
@@ -39,18 +38,4 @@ function scrollTo(hash: string) {
     behavior: 'smooth',
   })
   history.pushState(null, '', hash)
-}
-
-function themeToggle() {
-  const savedTheme = localStorage.getItem('theme')
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark-theme')
-  }
-
-  const toggleButton = document.getElementById('theme-toggle')
-  toggleButton?.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme')
-    const isDark = document.body.classList.contains('dark-theme')
-    localStorage.setItem('theme', isDark ? 'dark' : 'light')
-  })
 }
