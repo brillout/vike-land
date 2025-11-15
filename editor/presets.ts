fcf38b09c9e0ebd397fa669a06085ed3c18aa179
Romuald Brillout <git@brillout.com>
2025-11-15 13:18:54 +0100

    wip

--- editor/presets.ts
+++ editor/presets.ts
@@ -31,7 +31,7 @@ const colorsDefault: Colors = {
 const perspectiveDefault: Perspective = {
   rotation2D: 0,
   handleDiameter: 7.7,
-  handleLength: 10,
+  handleLength: 12.4,
   rotate: { x: 0.00, y: -7.16, z: -2 },
 }
 
