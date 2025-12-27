b123b767c1dae6e6376e2698c802d6efc146b8ab
Rom <git@brillout.com>
2025-12-27 22:38:11 +0100

    refactoring: decouple face gen (#8)

--- Hammer.ts
+++ Hammer.ts
@@ -442,28 +442,25 @@ function genFaces(head: Zdog.Anchor, ctx: Ctx) {
     addTo: head,
   })
 
-  // Front face
-  var frontFaceGroup = new Zdog.Group({
-    addTo: head,
-  })
+  // Back face
+  const faceBack = new Zdog.Group({ addTo: head })
   face2.copy({
     rotate: { y: (-1 * TAU) / 4 },
     translate: { x: 0, y: slopeSize, z: slopeSizeEnhanced - slopeSize },
+    addTo: faceBack,
+  })
+
+  // Front face
+  const faceFront = new Zdog.Group({ addTo: head })
+  face2.copy({
+    rotate: { y: (-1 * TAU) / 4, x: (-1 * TAU) / 2 },
+    translate: { x: 0, y: headLength - slopeSize, z: -(slopeSizeEnhanced - slopeSize) },
     color: normalizeColor(colors.colorFaceFront ?? colors.metalFace),
-    addTo: frontFaceGroup,
-  })
-  const lightningBolt = genLightningBolt(frontFaceGroup, colors, ctx)
-
-  // Back face
-  frontFaceGroup.copyGraph({
-    rotate: { x: (-1 * TAU) / 2 },
-    translate: { x: 0, y: headLength, z: 0 },
-    addTo: head,
+    addTo: faceFront,
   })
 
-  if (hideBackLightningBolt) {
-    lightningBolt.remove()
-  }
+  genLightningBolt(colors, ctx, true, { addTo: faceFront })
+  if (!hideBackLightningBolt) genLightningBolt(colors, ctx, false, { addTo: faceBack })
 }
 
 function genFaceSlopes(head: Zdog.Anchor, ctx: Ctx) {
@@ -642,7 +639,7 @@ function genHeadSide(
   return headSide
 }
 
-function genLightningBolt(group: Zdog.Group, colors: Colors, ctx: Ctx) {
+function genLightningBolt(colors: Colors, ctx: Ctx, isFront: boolean, props: Zdog.ShapeOptions) {
   const { slopeSize, slopeSizeEnhanced } = ctx
 
   //const width = 12
@@ -653,20 +650,18 @@ function genLightningBolt(group: Zdog.Group, colors: Colors, ctx: Ctx) {
   const translate = { x: -1 * (headLength / 2) + width / 2 , y: -2 * sideLength, z: sideLength + slopeSize + 2 }
   const rotate = undefined;
   /*/
-  const addTo = group
+  const invert = isFront ? -1 : 1
   const lightningBoltPosition = {
     x: lightningBoltOffset,
-    y: headLength / 2 - (slopeSizeEnhanced - slopeSize) / 2,
-    z: sideLength + slopeSizeEnhanced,
+    y: headLength / 2 - (invert * (slopeSizeEnhanced - slopeSize)) / 2,
+    z: invert * (sideLength + slopeSizeEnhanced),
   }
-  const rotate = { z: (1 * TAU) / 4 }
   //*/
   const stroke = 0.6
   // Distance from the hammer => apparent thickness of the lightning bolt
   const thikness = stroke / 4
   const shape = new Zdog.Shape({
-    addTo,
-    rotate,
+    rotate: { z: (1 * TAU) / 4, x: (-1 * TAU) / 2 },
     path: [
       { x: 54.616 - 67, y: 2.783 - 31 },
       { x: 54.652 - 67, y: 25.572 - 31 },
@@ -696,9 +691,10 @@ function genLightningBolt(group: Zdog.Group, colors: Colors, ctx: Ctx) {
     translate: {
       x: lightningBoltPosition.x,
       y: lightningBoltPosition.y,
-      z: lightningBoltPosition.z + thikness,
+      z: lightningBoltPosition.z + invert * thikness,
     },
     scale: { x: lightningBoltSize, y: lightningBoltSize, z: lightningBoltSize },
+    ...props,
   })
   return shape
 }
