084348b63115746cb8f5f94e7a7b5931803ba62e
Romuald Brillout <git@brillout.com>
2025-12-30 13:15:29 +0100

    fmt

--- Hammer.ts
+++ Hammer.ts
@@ -597,11 +597,11 @@ function genHeadSide(
 
   // Slope left
   if (!isFront || !isForScreenshot) {
-  slopeVertical({
-    translate: { z: sideLength },
-    scale: { x: sideLength, y: slopeSize, z: slopeSize },
-    color: normalizeColor(colors.colorSlopeLeft ?? colors.metalSlope),
-  })
+    slopeVertical({
+      translate: { z: sideLength },
+      scale: { x: sideLength, y: slopeSize, z: slopeSize },
+      color: normalizeColor(colors.colorSlopeLeft ?? colors.metalSlope),
+    })
   }
 
   // Slope right
